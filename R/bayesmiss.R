e5e1d53f2db09b0360d874f645931a5b59f9c268
jwb133 <jonathan.bartlett@lshtm.ac.uk>
2015-11-16 15:44:02 +0000

    Adding functionality to fit Poisson substantive model. Also modifications to allow for the possibility that no substantive model covariates have missing values, and therefore do not need models specified for them.

--- R/bayesmiss.R
+++ R/bayesmiss.R
@@ -61,15 +61,20 @@ bayesmiss <- function(originaldata,smtype,smformula,method,order) {
   #create matrix of response indicators
   r <- 1*(is.na(originaldata)==0)
 
-  if (sum(method!="") != max(order)) stop("Number of variables with imputation methods, as specified in method argument, does not concur with value given to order argument.")
-
   #outcome model
   outcomeCol <- which(colnames(originaldata)==as.formula(smformula)[[2]])
   outcomename <- as.character(as.formula(smformula)[[2]])
-  outcomeModDist <- paste("      ",outcomename,"[i] ~ dnorm(mu[i], outcome_tau)",sep="")
+
   omcovnames <- attr(terms(as.formula(smformula)), "term.labels")
-  omDist <- paste("      ",outcomename,"[i] ~ dnorm(mu[i], outcome_tau)",sep="")
-  omLinPred <- "      mu[i] <- beta[1]"
+  if (smtype=="lm") {
+    omDist <- paste("      ",outcomename,"[i] ~ dnorm(mu[i], outcome_tau)",sep="")
+    omLinPred <- "      mu[i] <- beta[1]"
+  }
+  else if (smtype=="poisson") {
+    omDist <- paste("      ",outcomename,"[i] ~ dpois(mu[i])",sep="")
+    omLinPred <- "      log(mu[i]) <- beta[1]"
+  }
+  else stop("You must supply a valid value as the smtype argument.")
   for (i in 1:length(omcovnames)) {
     omLinPred <- paste(omLinPred, " + beta[",i+1,"]*", omcovnames[i], "[i]", sep="")
   }
@@ -80,7 +85,12 @@ bayesmiss <- function(originaldata,smtype,smformula,method,order) {
     "   for (i in 1:n) {",
     omDist,
     omLinPred)
-  priorCode <- c("   beta ~ dmnorm(betamean,betaprec)","   outcome_tau ~ dgamma(tau_alpha, tau_beta)")
+  if (smtype=="lm") {
+    priorCode <- c("   beta ~ dmnorm(betamean,betaprec)","   outcome_tau ~ dgamma(tau_alpha, tau_beta)")
+  }
+  else if (smtype=="poisson") {
+    priorCode <- "   beta ~ dmnorm(betamean,betaprec)"
+  }
 
   fullObsVars <- which((colSums(r)==n) & (colnames(originaldata) %in% omcovnames))
 
@@ -89,139 +99,141 @@ bayesmiss <- function(originaldata,smtype,smformula,method,order) {
   rScriptText <- c(rScriptText,paste("jags.data <- as.list(",deparse(substitute(originaldata)),")", sep=""),
   "jags.data$n <- n",
   paste("jags.data$betamean <- rep(0, ",length(omcovnames)+1,")",sep=""),
-  paste("jags.data$betaprec <- 0.0001*diag(",length(omcovnames)+1,")",sep=""),
-  "jags.data$tau_alpha <- 0.5",
-  "jags.data$tau_beta <- 0.5")
+  paste("jags.data$betaprec <- 0.0001*diag(",length(omcovnames)+1,")",sep=""))
+  if (smtype=="lm") {
+    rScriptText <- c(rScriptText,"jags.data$tau_alpha <- 0.5","jags.data$tau_beta <- 0.5")
+  }
 
   #models for partially observed covariates
   numPartialVars <- max(order)
   prevMissVars <- vector(mode="integer",0)
 
-  for (var in 1:numPartialVars) {
-    targetCol <- which(order==var)
-    missName <- colnames(originaldata)[targetCol]
-    missCovNames <- colnames(originaldata)[c(fullObsVars,prevMissVars)]
+  if (numPartialVars>0) {
+    for (var in 1:numPartialVars) {
+      targetCol <- which(order==var)
+      missName <- colnames(originaldata)[targetCol]
+      missCovNames <- colnames(originaldata)[c(fullObsVars,prevMissVars)]
 
-    if (method[targetCol]=="norm") {
-      missDist <- paste("      ",missName,"[i] ~ dnorm(mu_",missName,"[i], tau_",missName,")", sep="")
-      missLinPred <- paste("      ","mu_",missName,"[i] <- gamma_",missName,"[1]", sep="")
-    }
-    else if (method[targetCol]=="logit") {
-      missDist <- paste("      ",missName,"[i] ~ dbern(mu_",missName,"[i])", sep="")
-      missLinPred <- paste("      ","logit(mu_",missName,"[i]) <- gamma_",missName,"[1]", sep="")
-    }
-    else if (method[targetCol]=="pois") {
-      missDist <- paste("      ",missName,"[i] ~ dpois(mu_",missName,"[i])", sep="")
-      missLinPred <- paste("      ","log(mu_",missName,"[i]) <- gamma_",missName,"[1]", sep="")
-    }
-    else if (method[targetCol]=="mlogit") {
-      missDist <- paste("      ",missName,"[i] ~ dcat(pi_",missName,"[i,])", sep="")
-      numCats <- nlevels(originaldata[,targetCol])
-
-      missLinPred <- paste("      ","pi_",missName,"[i,1] <- 1", sep="")
-      for (catNum in 2:numCats) {
-        missLinPred <- paste(missLinPred, " - pi_",missName,"[i,",catNum,"]", sep="")
-      }
-      for (catNum in 2:numCats) {
-        missLinPred <- c(missLinPred, paste("      ","pi_",missName,"[i,",catNum,"] <- exp(xb_",missName,"[i,",catNum-1,"])/denom_",missName,"[i]", sep=""))
-      }
-      denomExpr <- paste("      ","denom_",missName,"[i] <- 1", sep="")
-      for (catNum in 2:numCats) {
-        denomExpr <- paste(denomExpr,"+exp(xb_",missName,"[i,",catNum-1,"])", sep="")
-      }
-      missLinPred <- c(missLinPred,denomExpr)
-      for (catNum in 2:numCats) {
-        xbExpr <- paste("      ","xb_",missName,"[i,",catNum-1,"] <- gamma_",missName,"_",catNum-1,"[1]", sep="")
-        if (length(missCovNames)>0) {
-          for (i in 1:length(missCovNames)) {
-            xbExpr <- paste(xbExpr, " + gamma_",missName,"_",catNum-1,"[",i+1,"]*", missCovNames[i], "[i]", sep="")
-          }
-        }
-        missLinPred <- c(missLinPred, xbExpr)
-      }
-    }
-    else if (method[targetCol]=="ologit") {
-      missDist <- paste("      ",missName,"[i] ~ dcat(pi_",missName,"[i,])", sep="")
-      numCats <- nlevels(originaldata[,targetCol])
-
-      missLinPred <- paste("      ","xb_",missName,"[i] <- 0", sep="")
-      if (length(missCovNames)>0) {
-        for (i in 1:length(missCovNames)) {
-          missLinPred <- paste(missLinPred, " + gamma_",missName,"[",i,"]*", missCovNames[i], "[i]", sep="")
-        }
-      }
-      piExpr <- paste("      ","pi_",missName,"[i,1] <- 1", sep="")
-      for (catNum in 2:numCats) {
-        piExpr <- paste(piExpr, " - pi_",missName,"[i,",catNum,"]", sep="")
-      }
-      for (catNum in 2:(numCats-1)) {
-        piExpr <- c(piExpr, paste("      ","pi_",missName,"[i,",catNum,"] <- 1/(1+exp(-k_",missName,"_",catNum," + xb_",
-                                  missName,"[i])) - 1/(1+exp(-k_",missName,"_",catNum-1," + xb_",missName,"[i]))", sep=""))
-      }
-      piExpr <- c(piExpr, paste("      ","pi_",missName,"[i,",numCats,"] <- 1 - 1/(1+exp(-k_",missName,"_",numCats-1," + xb_",missName,"[i]))", sep=""))
-      missLinPred <- c(missLinPred, piExpr)
-    }
-    else stop(paste("Method ",method[targetCol]," not recognised.",sep=""))
-
-
-    if ((method[targetCol]=="norm") | (method[targetCol]=="logit") | (method[targetCol]=="pois")) {
-      if (length(missCovNames)>0) {
-        for (i in 1:length(missCovNames)) {
-          missLinPred <- paste(missLinPred, " + gamma_",missName,"[",i+1,"]*", missCovNames[i], "[i]", sep="")
-        }
-      }
-    }
-
-    #append to modelCode
-    modelCode <- c(modelCode,"",missDist,missLinPred)
-
-    #append to priorCode
-    if (method[targetCol]=="mlogit") {
-      priorCode <- c(priorCode,"")
-      for (catNum in 2:numCats) {
-        priorCode <- c(priorCode,paste("   gamma_",missName,"_",catNum-1," ~ dmnorm(gamma_",missName,"_",catNum-1,"_mean,gamma_",missName,"_",catNum-1,"_prec)", sep=""))
-      }
-    }
-    else {
-      priorCode <- c(priorCode,"",paste("   gamma_",missName," ~ dmnorm(gamma_",missName,"_mean,gamma_",missName,"_prec)", sep=""))
       if (method[targetCol]=="norm") {
-        priorCode <- c(priorCode, paste("   tau_",missName," ~ dgamma(tau_alpha, tau_beta)", sep=""))
+        missDist <- paste("      ",missName,"[i] ~ dnorm(mu_",missName,"[i], tau_",missName,")", sep="")
+        missLinPred <- paste("      ","mu_",missName,"[i] <- gamma_",missName,"[1]", sep="")
+      }
+      else if (method[targetCol]=="logit") {
+        missDist <- paste("      ",missName,"[i] ~ dbern(mu_",missName,"[i])", sep="")
+        missLinPred <- paste("      ","logit(mu_",missName,"[i]) <- gamma_",missName,"[1]", sep="")
+      }
+      else if (method[targetCol]=="pois") {
+        missDist <- paste("      ",missName,"[i] ~ dpois(mu_",missName,"[i])", sep="")
+        missLinPred <- paste("      ","log(mu_",missName,"[i]) <- gamma_",missName,"[1]", sep="")
+      }
+      else if (method[targetCol]=="mlogit") {
+        missDist <- paste("      ",missName,"[i] ~ dcat(pi_",missName,"[i,])", sep="")
+        numCats <- nlevels(originaldata[,targetCol])
+
+        missLinPred <- paste("      ","pi_",missName,"[i,1] <- 1", sep="")
+        for (catNum in 2:numCats) {
+          missLinPred <- paste(missLinPred, " - pi_",missName,"[i,",catNum,"]", sep="")
+        }
+        for (catNum in 2:numCats) {
+          missLinPred <- c(missLinPred, paste("      ","pi_",missName,"[i,",catNum,"] <- exp(xb_",missName,"[i,",catNum-1,"])/denom_",missName,"[i]", sep=""))
+        }
+        denomExpr <- paste("      ","denom_",missName,"[i] <- 1", sep="")
+        for (catNum in 2:numCats) {
+          denomExpr <- paste(denomExpr,"+exp(xb_",missName,"[i,",catNum-1,"])", sep="")
+        }
+        missLinPred <- c(missLinPred,denomExpr)
+        for (catNum in 2:numCats) {
+          xbExpr <- paste("      ","xb_",missName,"[i,",catNum-1,"] <- gamma_",missName,"_",catNum-1,"[1]", sep="")
+          if (length(missCovNames)>0) {
+            for (i in 1:length(missCovNames)) {
+              xbExpr <- paste(xbExpr, " + gamma_",missName,"_",catNum-1,"[",i+1,"]*", missCovNames[i], "[i]", sep="")
+            }
+          }
+          missLinPred <- c(missLinPred, xbExpr)
+        }
       }
       else if (method[targetCol]=="ologit") {
-        #priors for cutpoints in ordinal logistic regression
-        priorCode <- c(priorCode, paste("   k_",missName,"_1 ~ dnorm(k_",missName,"_1_mean, k_",missName,"_1_prec)", sep=""))
+        missDist <- paste("      ",missName,"[i] ~ dcat(pi_",missName,"[i,])", sep="")
+        numCats <- nlevels(originaldata[,targetCol])
+
+        missLinPred <- paste("      ","xb_",missName,"[i] <- 0", sep="")
+        if (length(missCovNames)>0) {
+          for (i in 1:length(missCovNames)) {
+            missLinPred <- paste(missLinPred, " + gamma_",missName,"[",i,"]*", missCovNames[i], "[i]", sep="")
+          }
+        }
+        piExpr <- paste("      ","pi_",missName,"[i,1] <- 1", sep="")
+        for (catNum in 2:numCats) {
+          piExpr <- paste(piExpr, " - pi_",missName,"[i,",catNum,"]", sep="")
+        }
         for (catNum in 2:(numCats-1)) {
-          priorCode <- c(priorCode, paste("   k_",missName,"_",catNum," <- k_",missName,"_1 + kinc_",missName,"_",catNum-1,  sep=""))
-          priorCode <- c(priorCode, paste("   kinc_",missName,"_",catNum-1," ~ dlnorm(kinc_",missName,"_",catNum-1,"_mean, kinc_",missName,"_",catNum-1,"_prec)",sep=""))
+          piExpr <- c(piExpr, paste("      ","pi_",missName,"[i,",catNum,"] <- 1/(1+exp(-k_",missName,"_",catNum," + xb_",
+                                    missName,"[i])) - 1/(1+exp(-k_",missName,"_",catNum-1," + xb_",missName,"[i]))", sep=""))
+        }
+        piExpr <- c(piExpr, paste("      ","pi_",missName,"[i,",numCats,"] <- 1 - 1/(1+exp(-k_",missName,"_",numCats-1," + xb_",missName,"[i]))", sep=""))
+        missLinPred <- c(missLinPred, piExpr)
+      }
+      else stop(paste("Method ",method[targetCol]," not recognised.",sep=""))
+
+
+      if ((method[targetCol]=="norm") | (method[targetCol]=="logit") | (method[targetCol]=="pois")) {
+        if (length(missCovNames)>0) {
+          for (i in 1:length(missCovNames)) {
+            missLinPred <- paste(missLinPred, " + gamma_",missName,"[",i+1,"]*", missCovNames[i], "[i]", sep="")
+          }
         }
       }
-    }
 
-    #append priors to JAGS data list
-    if (method[targetCol]=="mlogit") {
-      for (catNum in 2:numCats) {
-        rScriptText <- c(rScriptText, paste("jags.data$gamma_",missName,"_",catNum-1,"_mean <- rep(0, ",length(missCovNames)+1,")", sep=""),
-                         paste("jags.data$gamma_",missName,"_",catNum-1,"_prec <- 0.0001*diag(",length(missCovNames)+1,")", sep=""))
-      }
-    }
-    else if (method[targetCol]=="ologit") {
-      rScriptText <- c(rScriptText, paste("jags.data$gamma_",missName,"_mean <- rep(0, ",length(missCovNames),")", sep=""),
-                       paste("jags.data$gamma_",missName,"_prec <- 0.0001*diag(",length(missCovNames),")", sep=""))
-      rScriptText <- c(rScriptText, paste("jags.data$k_",missName,"_1_mean <- 0", sep=""))
-      rScriptText <- c(rScriptText, paste("jags.data$k_",missName,"_1_prec <- 0.0001", sep=""))
-      for (catNum in 2:(numCats-1)) {
-        rScriptText <- c(rScriptText, paste("jags.data$kinc_",missName,"_",catNum-1,"_mean <- 0", sep=""))
-        rScriptText <- c(rScriptText, paste("jags.data$kinc_",missName,"_",catNum-1,"_prec <- 0.0001", sep=""))
-      }
-    }
-    else {
-      rScriptText <- c(rScriptText, paste("jags.data$gamma_",missName,"_mean <- rep(0, ",length(missCovNames)+1,")", sep=""),
-        paste("jags.data$gamma_",missName,"_prec <- 0.0001*diag(",length(missCovNames)+1,")", sep=""))
-    }
+      #append to modelCode
+      modelCode <- c(modelCode,"",missDist,missLinPred)
 
-    prevMissVars <- c(prevMissVars,targetCol)
+      #append to priorCode
+      if (method[targetCol]=="mlogit") {
+        priorCode <- c(priorCode,"")
+        for (catNum in 2:numCats) {
+          priorCode <- c(priorCode,paste("   gamma_",missName,"_",catNum-1," ~ dmnorm(gamma_",missName,"_",catNum-1,"_mean,gamma_",missName,"_",catNum-1,"_prec)", sep=""))
+        }
+      }
+      else {
+        priorCode <- c(priorCode,"",paste("   gamma_",missName," ~ dmnorm(gamma_",missName,"_mean,gamma_",missName,"_prec)", sep=""))
+        if (method[targetCol]=="norm") {
+          priorCode <- c(priorCode, paste("   tau_",missName," ~ dgamma(tau_alpha, tau_beta)", sep=""))
+        }
+        else if (method[targetCol]=="ologit") {
+          #priors for cutpoints in ordinal logistic regression
+          priorCode <- c(priorCode, paste("   k_",missName,"_1 ~ dnorm(k_",missName,"_1_mean, k_",missName,"_1_prec)", sep=""))
+          for (catNum in 2:(numCats-1)) {
+            priorCode <- c(priorCode, paste("   k_",missName,"_",catNum," <- k_",missName,"_1 + kinc_",missName,"_",catNum-1,  sep=""))
+            priorCode <- c(priorCode, paste("   kinc_",missName,"_",catNum-1," ~ dlnorm(kinc_",missName,"_",catNum-1,"_mean, kinc_",missName,"_",catNum-1,"_prec)",sep=""))
+          }
+        }
+      }
+
+      #append priors to JAGS data list
+      if (method[targetCol]=="mlogit") {
+        for (catNum in 2:numCats) {
+          rScriptText <- c(rScriptText, paste("jags.data$gamma_",missName,"_",catNum-1,"_mean <- rep(0, ",length(missCovNames)+1,")", sep=""),
+                           paste("jags.data$gamma_",missName,"_",catNum-1,"_prec <- 0.0001*diag(",length(missCovNames)+1,")", sep=""))
+        }
+      }
+      else if (method[targetCol]=="ologit") {
+        rScriptText <- c(rScriptText, paste("jags.data$gamma_",missName,"_mean <- rep(0, ",length(missCovNames),")", sep=""),
+                         paste("jags.data$gamma_",missName,"_prec <- 0.0001*diag(",length(missCovNames),")", sep=""))
+        rScriptText <- c(rScriptText, paste("jags.data$k_",missName,"_1_mean <- 0", sep=""))
+        rScriptText <- c(rScriptText, paste("jags.data$k_",missName,"_1_prec <- 0.0001", sep=""))
+        for (catNum in 2:(numCats-1)) {
+          rScriptText <- c(rScriptText, paste("jags.data$kinc_",missName,"_",catNum-1,"_mean <- 0", sep=""))
+          rScriptText <- c(rScriptText, paste("jags.data$kinc_",missName,"_",catNum-1,"_prec <- 0.0001", sep=""))
+        }
+      }
+      else {
+        rScriptText <- c(rScriptText, paste("jags.data$gamma_",missName,"_mean <- rep(0, ",length(missCovNames)+1,")", sep=""),
+          paste("jags.data$gamma_",missName,"_prec <- 0.0001*diag(",length(missCovNames)+1,")", sep=""))
+      }
+
+      prevMissVars <- c(prevMissVars,targetCol)
+    }
   }
-
 
   #write model to file
   modFileName <- "bayesmissmod.bug"
